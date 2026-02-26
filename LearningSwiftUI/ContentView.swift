3ab5135aca18bfbeb423af22db0474bc729d7ff9
DM <55935266+dm-for-git@users.noreply.github.com>
2026-02-26 22:06:08 +0700

    Initial Commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  Created by David Martin on 2026-02-26.
+//  
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,15 @@
+//
+//  Created by David Martin on 2026-02-26.
+//  
+
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
